f61c605b901e6b2de8af77fb22559169d62cf7bf
이연지 <yoenjii310@gmail.com>
2020-11-18 03:49:13 +0900

    create manager Attendance 1

--- Project/app/src/main/java/com/example/dotory/manager/ManagerAttendanceEnterActivity.java
+++ Project/app/src/main/java/com/example/dotory/manager/ManagerAttendanceEnterActivity.java
@@ -2,15 +2,77 @@ package com.example.dotory.manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.example.dotory.R;
+import com.example.dotory.student.StudentAttendanceEnterActivity;
+import com.example.dotory.student.StudentAttendanceGoOutActivity;
 
 public class ManagerAttendanceEnterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_manager_attendance_enter);
+    }
+
+
+    public void menuBoardOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerBoardActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+    public void menuAttendanceOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerAttendanceEnterActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+    public void menuPointOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerPointActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+    public void menuStayOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerStayChoiceActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+    public void menuInformationOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerInformationActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+
+
+    public void attendanceEnterOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerAttendanceEnterActivity.class);
+
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+    public void attendanceGoOutOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerAttendanceGoOutActivity.class);
+
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
     }
 }
--- Project/app/src/main/java/com/example/dotory/manager/ManagerInformationActivity.java
+++ Project/app/src/main/java/com/example/dotory/manager/ManagerInformationActivity.java
@@ -2,7 +2,10 @@ package com.example.dotory.manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.example.dotory.R;
 
@@ -11,6 +14,45 @@ public class ManagerInformationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_manager_information);
+    }
+
+
+    public void menuBoardOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerBoardActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+    public void menuAttendanceOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerAttendanceEnterActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+    public void menuPointOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerPointActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+    public void menuStayOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerStayChoiceActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
+    }
+    public void menuInformationOnClick(View view)
+    {
+        Intent intent = new Intent(this, ManagerInformationActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+        finish();
     }
 }
